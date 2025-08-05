5ac866627fcd00c5481d6bbf7fb88b46d5b83935
David-ProgramerJ <davidprogramer9@gmail.com>
2025-08-05 15:13:41 -0500

    Secion token

--- FRONTEND/src/api/AuthApi.js
+++ FRONTEND/src/api/AuthApi.js
@@ -1,7 +1,7 @@
 import axiosInstance from './axiosInstance';
 
 export const login = async (email, password) => {
-  const response = await axiosInstance.post('/usuario/login/', {
+  const response = await axiosInstance.post('/http://127.0.0.1:8000/api/token/', {
     email,
     password,
   });
