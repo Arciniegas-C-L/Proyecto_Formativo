d691d2c9b3d0051c04e903b29ce00d12a96c53c8
Arciniegas-C-L <arciniegas.cristian2341@gmail.com>
2025-09-20 14:22:55 -0500

    Solucion error de page

--- FRONTEND/src/pages/Categoriaspage.jsx
+++ FRONTEND/src/pages/Categoriaspage.jsx
@@ -1,6 +1,5 @@
 //Este componente es la página de Categorias
 //Aqui podemos unir todos los componentes o subcomponentes para exportar a la app.jsx
-
 import { CategoriaForm } from "../components/Categorias/Categorias";
 
 export function CategoriasPage() {
