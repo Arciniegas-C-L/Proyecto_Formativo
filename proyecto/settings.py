0f4f0ca1abb840f83e65825fcb8428ff03a884ab
Arciniegas-C-L <arciniegas.cristian2341@gmail.com>
2025-09-20 16:57:11 -0500

    revision de error en login 1

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -200,3 +200,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+# Asegura autenticación por correo
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
