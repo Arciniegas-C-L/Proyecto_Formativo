05c87419da4d7993a5f152bf5036ccca1c336f82
Arciniegas-C-L <arciniegas.cristian2341@gmail.com>
2025-09-20 23:12:29 -0500

    modificacion de gmail

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -197,8 +197,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "variedadezyestiloszoe@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "csit fham irwi kdpy")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
