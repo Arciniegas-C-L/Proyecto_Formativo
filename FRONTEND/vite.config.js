d6676743dc696d6060b051456ad01b81a29bcdc4
Camila-Hernandez-103 <camihernandez7856@gmail.com>
2025-09-23 09:58:05 -0500

    ultimos cmabios

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // ⚠️ cambia esto por tu subdominio real:
-const NGROK_HOST = 'collar-reviewed-norman-trailers.trycloudflare.com';
+const NGROK_HOST = 'competitive-analyst-dream-yours.trycloudflare.com';
 
 export default defineConfig({
   plugins: [react()],
