69c0dc4b5230c13b2f67d43af4c8bf0b7d45dde4
David-ProgramerJ <davidprogramer9@gmail.com>
2025-09-23 08:00:47 -0500

    Join try camila

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // ⚠️ cambia esto por tu subdominio real:
-const NGROK_HOST = 'reviews-introductory-drink-barnes.trycloudflare.com'
+const NGROK_HOST = 'menus-keith-spas-aside.trycloudflare.com'
 
 export default defineConfig({
   plugins: [react()],
