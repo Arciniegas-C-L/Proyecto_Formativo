7a1e25a29bf2f4a2805c6f15ab0322bd2f7ffca9
Camila-Hernandez-103 <camihernandez7856@gmail.com>
2025-09-28 18:25:23 -0500

    base de datos

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // ⚠️ cambia esto por tu subdominio real:
-const NGROK_HOST = 'length-agrees-salad-norfolk.trycloudflare.com'
+const NGROK_HOST = 'genesis-conventional-quantitative-costumes.trycloudflare.com'
 
 export default defineConfig({
   plugins: [react()],
